f2d2d142ef1ff57126373e64ff09541c13673072
Alys <alice.g.cooper@gmail.com>
2022-04-27 18:13:46 -0400

    Update button IDs and remove factorial option

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 const power = (a,b) => a ** b;
-const factorial = (num) => {
-  // factorial needs to list all numbers between x and 1, else 1
-  if (num === 0 || num === 1)
-    return 1;
-  for (let i = num - 1; i >= 1; i--) {
-    num *= i;
-  }
-  return num;
-}
 
 function operate(operator, a, b) {
     return operator(a, b);
 }
+// Create the functions that populate the display when 
+// you click the number buttons… you should be storing 
+// the ‘display value’ in a variable somewhere for use 
+// in the next step.
+
+let displayValue;
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
