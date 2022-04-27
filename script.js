2797a1ecc865d36c5640b72a436f6f34b5128574
Alys <alice.g.cooper@gmail.com>
2022-04-27 17:17:46 -0400

    Work on operate function

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-// from earlier exercise
-
-const add = function(a,b) {
-	return a + b;
-};
-
-const subtract = function(a,b) {
-	return a - b;
-};
-
-const multiply = function(a,b) {
-  return a * b;
-};
-
-const divide = function(a,b) {
-    return a / b;
-}
-
-const power = function(a,b) {
-	return a ** b;
-};
-
-const factorial = function(num) {
-	// factorial needs to list all numbers between x and 1, else 1
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+const power = (a,b) => a ** b;
+const factorial = (num) => {
+  // factorial needs to list all numbers between x and 1, else 1
   if (num === 0 || num === 1)
     return 1;
   for (let i = num - 1; i >= 1; i--) {
     num *= i;
   }
   return num;
+}
+
+function operate(a,operator,b) {
+  if (operator === "+") {
+    return add(a,b);
+  } else if (operator === "-") {
+    return subtract(a,b);
+  } else if (operator === "*") {
+      return multiply(a,b);
+  } else if (operator === "/") {
+    return divide(a,b);  
+  } else if (operator === "**") {
+    return power(a,b);
+  } else {
+  return "OOPS";
+  }
 }
