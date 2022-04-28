d71fea1722077a4aa7fc7aa84bfc4d9d483e8ac1
Alys <alice.g.cooper@gmail.com>
2022-04-28 10:57:44 -0400

    Add in event listener for digits

--- script.js
+++ script.js
@@ -13,6 +13,14 @@ function operate(operator, a, b) {
 // in the next step.
 
 let displayValue;
+// digit buttons
+const digits = document.querySelectorAll(".digits");
+// add click function to all digits
+  digits.forEach(digit => digit.addEventListener('click', function(e) {
+      digit.style.background = "blueviolet";
+  })
+);
+
 // key selectors
 const zero = document.querySelector("#zero");
 const one = document.querySelector("#one");
@@ -25,7 +33,14 @@ const seven = document.querySelector("#seven");
 const eight = document.querySelector("#eight");
 const nine = document.querySelector("#nine");
 
+// create the function(S) that populate the display when click the number buttons
+// create eventListener for click to add X value to display
+// can use key-value to set numbers per one? so one function per digit button?
+  // remember to add in loop to add it to all buttons
+
+
 //REMEMBER when we type things in we want to append the text to the display textContent
+// display.textContent += 
 
 //display selector
 let display = document.querySelector("#display");
